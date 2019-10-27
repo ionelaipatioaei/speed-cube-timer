03ecd7d1427f8ae17196f0a3767bf2edbdb0e613
Ionel Aipatioaei <lionel.developer72@gmail.com>
2019-10-27 11:15:43 +0200

    basic file structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'package:speed_cube_timer/screens/home.dart';
+import 'package:flutter/services.dart';
+import 'package:speed_cube_timer/screens/home/home.dart';
 
 void main() async {
 
@@ -9,6 +12,11 @@ void main() async {
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      // for some reason these values are switched
+      statusBarBrightness: Platform.isIOS ? Brightness.dark : Brightness.light,
+    ));
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Speed Cube Timer",
