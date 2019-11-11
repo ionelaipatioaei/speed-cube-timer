7f3f00a8c7f113a4896a2d1834d40515ae4dc0c4
Ionel Aipatioaei <lionel.developer72@gmail.com>
2019-11-11 03:03:43 +0200

    moved iap directly to settings state because I couldn't abstract the login in a class

--- lib/screens/settings/settings.dart
+++ lib/screens/settings/settings.dart
@@ -1,7 +1,11 @@
+import 'dart:async';
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:in_app_purchase/in_app_purchase.dart';
 import 'package:speed_cube_timer/components/common/double_text_button.dart';
 import 'package:speed_cube_timer/components/common/text_button.dart';
 import 'package:speed_cube_timer/components/containers/item_view.dart';
@@ -17,7 +21,6 @@ import 'package:speed_cube_timer/screens/info/credits.dart';
 import 'package:speed_cube_timer/screens/info/privacy_policy.dart';
 import 'package:speed_cube_timer/screens/settings/select_practice.dart';
 import 'package:speed_cube_timer/shared/background.dart';
-import 'package:speed_cube_timer/utils/buy_iap.dart';
 import 'package:speed_cube_timer/utils/gen_scramble.dart';
 import 'package:speed_cube_timer/utils/iap_config.dart';
 
@@ -35,11 +38,93 @@ class _SettingsState extends State<Settings> {
   bool showScramblingSequence;
   int scramblingSequenceLength;
 
-  BuyIAP buyIAP = BuyIAP(() => IAPConfig.confirmPurchase());
+  static const List<String> productIds = ["remove_ads_unlock_all_customizations"];
+
+  bool available = true;
+  InAppPurchaseConnection iap = InAppPurchaseConnection.instance;
+  List<ProductDetails> products = [];
+  List<PurchaseDetails> purchases = [];
+  StreamSubscription subscription;
+
+  void initialize() async {
+    available = await iap.isAvailable();
+    if (available) {
+      await getProducts();
+      await getPastPurchases();
+
+      verifyPurchase();
+    }
+
+    subscription = iap.purchaseUpdatedStream.listen((data) => setState(() {
+      print("new purchase!!!");
+      print("new purchase!!!");
+      print("new purchase!!!");
+      print("new purchase!!!");
+      print("new purchase!!!");
+      print("new purchase!!!");
+      print("new purchase!!!");
+      print("new purchase!!!");
+      print("new purchase!!!");
+      purchases.addAll(data);
+      verifyPurchase();
+    }));
+  }
+
+  Future<void> getProducts() async {
+    Set<String> ids = Set.from(productIds);
+    ProductDetailsResponse response = await iap.queryProductDetails(ids);
+    setState(() =>products = response.productDetails);
+  }
+
+  Future<void> getPastPurchases() async {
+    QueryPurchaseDetailsResponse response = await iap.queryPastPurchases();
+    for (PurchaseDetails purchase in response.pastPurchases) {
+      if (Platform.isIOS) {
+        InAppPurchaseConnection.instance.completePurchase(purchase);
+      }
+    }
+    setState(() => purchases = response.pastPurchases);
+  }
+
+  PurchaseDetails hasPurchased(String productID) {
+    return purchases.firstWhere((PurchaseDetails purchase) => purchase.productID == productID, orElse: () => null);
+  }
+
+  void verifyPurchase() {
+    PurchaseDetails removeAllAdsUnlockAllCustomizationsPurchase = hasPurchased(productIds[0]);
+    if (removeAllAdsUnlockAllCustomizationsPurchase != null && removeAllAdsUnlockAllCustomizationsPurchase.status == PurchaseStatus.purchased) {
+      print("accepted!");
+      print("accepted!");
+      print("accepted!");
+      print("accepted!");
+      print("accepted!");
+      print("accepted!");
+      print("accepted!");
+      print("accepted!");
+      print("accepted!");
+      print("accepted!");
+      print("accepted!");
+      print("accepted!");
+      print("accepted!");
+      print("accepted!");
+      print("accepted!");
+      print("accepted!");
+      print("accepted!");
+      print("accepted!");
+      print("accepted!");
+      print("accepted!");
+      IAPConfig.confirmPurchase();
+    }
+  }
+
+  void buyRemoveAllAdsUnlockAllCustomizations() {
+    final PurchaseParam purchaseParam = PurchaseParam(productDetails: products[0]);
+    iap.buyConsumable(purchaseParam: purchaseParam, autoConsume: true);
+  }
 
   @override
   void initState() {
-    buyIAP.initialize();
+    initialize();
     liveStopwatch = settings.get("live_stopwatch", defaultValue: true);
     allowInspectionTime = settings.get("allow_inspection_time", defaultValue: false);
     inspectionTime = settings.get("inspection_time", defaultValue: 15000);
@@ -123,7 +208,7 @@ class _SettingsState extends State<Settings> {
                   TextButton("Credits", width, () => Navigator.push(context, CupertinoPageRoute(builder: (BuildContext context) => Credits()))),
                   CustomSpacer(width),
 
-                  DoubleTextButton("Remove All Ads &", "Unlock All Customizations", width, () => buyIAP.buyRemoveAllAdsUnlockAllCustomizations()),
+                  DoubleTextButton("Remove All Ads &", "Unlock All Customizations", width, () => buyRemoveAllAdsUnlockAllCustomizations()),
                 ]
               )
             )
--- lib/utils/buy_iap.dart
+++ lib/utils/buy_iap.dart
@@ -1,65 +1,62 @@
+// UNUSED CODE
+
 import 'dart:async';
 import 'dart:io';
 
 import 'package:in_app_purchase/in_app_purchase.dart';
+import 'package:speed_cube_timer/utils/iap_config.dart';
 
-class BuyIAP {
-  static const List<String> productIds = ["remove_ads_unlock_all_customizations"];
+const List<String> productIds = ["remove_ads_unlock_all_customizations"];
 
-  bool available = true;
-  InAppPurchaseConnection iap = InAppPurchaseConnection.instance;
-  List<ProductDetails> products = [];
-  List<PurchaseDetails> purchases = [];
-  StreamSubscription subscription;
+bool available = true;
+InAppPurchaseConnection iap = InAppPurchaseConnection.instance;
+List<ProductDetails> products = [];
+List<PurchaseDetails> purchases = [];
+StreamSubscription subscription;
 
-  final Function confirm;
+void initialize() async {
+  available = await iap.isAvailable();
+  if (available) {
+    await getProducts();
+    await getPastPurchases();
 
-  BuyIAP(this.confirm);
-
-  void initialize() async {
-    available = await iap.isAvailable();
-    if (available) {
-      await getProducts();
-      await getPastPurchases();
-
-      verifyPurchase();
-    }
-
-    subscription = iap.purchaseUpdatedStream.listen((data) => () {
-      purchases.addAll(data);
-      verifyPurchase();
-    });
+    verifyPurchase();
   }
 
-  Future<void> getProducts() async {
-    Set<String> ids = Set.from(productIds);
-    ProductDetailsResponse response = await iap.queryProductDetails(ids);
-    products = response.productDetails;
-  }
+  subscription = iap.purchaseUpdatedStream.listen((data) => () {
+    purchases.addAll(data);
+    verifyPurchase();
+  });
+}
 
-  Future<void> getPastPurchases() async {
-    QueryPurchaseDetailsResponse response = await iap.queryPastPurchases();
-    for (PurchaseDetails purchase in response.pastPurchases) {
-      if (Platform.isIOS) {
-        InAppPurchaseConnection.instance.completePurchase(purchase);
-      }
-    }
-    purchases = response.pastPurchases;
-  }
+Future<void> getProducts() async {
+  Set<String> ids = Set.from(productIds);
+  ProductDetailsResponse response = await iap.queryProductDetails(ids);
+  products = response.productDetails;
+}
 
-  PurchaseDetails hasPurchased(String productID) {
-    return purchases.firstWhere((PurchaseDetails purchase) => purchase.productID == productID, orElse: () => null);
-  }
-
-  void verifyPurchase() {
-    PurchaseDetails removeAllAdsUnlockAllCustomizationsPurchase = hasPurchased(productIds[0]);
-    if (removeAllAdsUnlockAllCustomizationsPurchase != null && removeAllAdsUnlockAllCustomizationsPurchase.status == PurchaseStatus.purchased) {
-      confirm();
+Future<void> getPastPurchases() async {
+  QueryPurchaseDetailsResponse response = await iap.queryPastPurchases();
+  for (PurchaseDetails purchase in response.pastPurchases) {
+    if (Platform.isIOS) {
+      InAppPurchaseConnection.instance.completePurchase(purchase);
     }
   }
+  purchases = response.pastPurchases;
+}
 
-  void buyRemoveAllAdsUnlockAllCustomizations() {
-    final PurchaseParam purchaseParam = PurchaseParam(productDetails: products[0]);
-    iap.buyConsumable(purchaseParam: purchaseParam, autoConsume: true);
+PurchaseDetails hasPurchased(String productID) {
+  return purchases.firstWhere((PurchaseDetails purchase) => purchase.productID == productID, orElse: () => null);
+}
+
+void verifyPurchase() {
+  PurchaseDetails removeAllAdsUnlockAllCustomizationsPurchase = hasPurchased(productIds[0]);
+  if (removeAllAdsUnlockAllCustomizationsPurchase != null && removeAllAdsUnlockAllCustomizationsPurchase.status == PurchaseStatus.purchased) {
+    IAPConfig.confirmPurchase();
   }
+}
+
+void buyRemoveAllAdsUnlockAllCustomizations() {
+  final PurchaseParam purchaseParam = PurchaseParam(productDetails: products[0]);
+  iap.buyConsumable(purchaseParam: purchaseParam, autoConsume: true);
 }
